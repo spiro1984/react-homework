b0b77ff12947f26362f665a4b9aa9e66ca16a697
spiro1984 <spirin.mail@gmail.com>
2023-03-13 21:40:10 +0100

    homework 1

---
+++ src/components/Button/Button.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.scss";
+
+const Button = ({
+  className,
+  isLanding = false,
+  isSecondary = false,
+  isOutline = false,
+  children,
+  ...props
+}) => {
+  let modifierClass = "";
+
+  switch (true) {
+    case isLanding:
+      modifierClass = "Button_landing";
+      break;
+    case isSecondary:
+      modifierClass = "Button_secondary";
+      break;
+    case isOutline:
+      modifierClass = "Button_outline";
+      break;
+    default:
+      modifierClass = "";
+      break;
+  }
+
+  return (
+    <button className={`Button ${modifierClass} ${className}`}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+  isLanding: PropTypes.bool,
+  isSecondary: PropTypes.bool,
+  isOutline: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+export default Button;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Landing from "../../components/Landing/Landing";
+
+const Home = (props) => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Landing />
+      </main>
+    </>
+  );
+};
+
+export default Home;
